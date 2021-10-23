bb0d392fef16d5e8784a0a26ea1f4b5ace156492
Jose Victor Persset <jpersset@gmail.com>
2021-10-23 16:15:51 -0300

    Add Non Binary Values Verification, CSS and finish

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 "use strict";
 function Bin2Dec() {
-    var binary = document.getElementById("input_bin").value;
+    document.getElementById("input-dec").value = "";
+    var binary = document.getElementById("input-bin").value;
+    var nonBinCounter = 0;
     var decimal = 0;
-    for (var i = binary.length - 1; i >= 0; i--) {
-        decimal =
-            decimal + parseInt(binary[i]) * Math.pow(2, binary.length - 1 - i);
-        console.log(decimal);
+    for (var i = 0; i < binary.length; i++) {
+        if (binary[i] != "0" && binary[i] != "1") {
+            nonBinCounter++;
+        }
     }
-    return (document.getElementById("input_dec").value =
-        decimal.toString());
+    if (nonBinCounter > 0) {
+        alert("Por favor insira apenas valores binários!");
+    }
+    else {
+        for (var i = binary.length - 1; i >= 0; i--) {
+            decimal =
+                decimal + Number(binary[i]) * Math.pow(2, binary.length - 1 - i);
+        }
+        return (document.getElementById("input-dec").value =
+            decimal.toString());
+    }
 }
--- app.ts
+++ app.ts
@@ -1,12 +1,23 @@
 function Bin2Dec() {
-  let binary = (document.getElementById("input_bin") as HTMLInputElement).value;
+  (document.getElementById("input-dec") as HTMLInputElement).value = "";
+  let binary = (document.getElementById("input-bin") as HTMLInputElement).value;
+  let nonBinCounter = 0;
   let decimal = 0;
 
-  for (let i = binary.length - 1; i >= 0; i--) {
-    decimal =
-      decimal + parseInt(binary[i]) * Math.pow(2, binary.length - 1 - i);
-    console.log(decimal);
+  for (let i = 0; i < binary.length; i++) {
+    if (binary[i] != "0" && binary[i] != "1") {
+      nonBinCounter++;
+    }
   }
-  return ((document.getElementById("input_dec") as HTMLInputElement).value =
-    decimal.toString());
+
+  if (nonBinCounter > 0) {
+    alert("Por favor insira apenas valores binários!");
+  } else {
+    for (let i = binary.length - 1; i >= 0; i--) {
+      decimal =
+        decimal + Number(binary[i]) * Math.pow(2, binary.length - 1 - i);
+    }
+    return ((document.getElementById("input-dec") as HTMLInputElement).value =
+      decimal.toString());
+  }
 }
